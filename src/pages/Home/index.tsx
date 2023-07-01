3636ff0662d4552194444b812a3fc9daf27e221e
diegonacif <diegoreisnacif@gmail.com>
2023-07-01 14:32:40 -0300

    chore: header styling

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+export function Home() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="taks">Vou trabalhar em</label>
+        <input type="text" id="task" />
+
+        <label htmlFor="minutesAmount">durante</label>
+        <input type="number" id="minutesAmount" />
+
+        <span>minutos.</span>
+      </form>
+
+      <button></button>
+    </div>
+  )
+}
