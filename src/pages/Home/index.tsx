b9a7a6104c55cf70d0cff7f6296eb6efc6d61cb7
diegonacif <diegoreisnacif@gmail.com>
2023-07-01 16:18:45 -0300

    chore: home component structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
 export function Home() {
   return (
-    <div>
+    <HomeContainer>
       <form action="">
-        <label htmlFor="taks">Vou trabalhar em</label>
-        <input type="text" id="task" />
+        <FormContainer>
+          <label htmlFor="taks">Vou trabalhar em</label>
+          <input type="text" id="task" />
 
-        <label htmlFor="minutesAmount">durante</label>
-        <input type="number" id="minutesAmount" />
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
 
-        <span>minutos.</span>
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
       </form>
-
-      <button></button>
-    </div>
+    </HomeContainer>
   )
 }
