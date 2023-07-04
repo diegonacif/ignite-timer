6abf2cb1ad016a4164ecd5a84ee7fea29f10b938
diegonacif <diegoreisnacif@gmail.com>
2023-07-04 11:51:38 -0300

    reafactor: cycle context provider component around the router

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
         <GlobalStyle />
       </ThemeProvider>
     </BrowserRouter>
